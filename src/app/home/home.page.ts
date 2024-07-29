2577de7e1784e8c5f2dfd374d79117764c95eb4c
Ken Sodemann <ken@ionic.io>
2024-07-29 13:19:48 -0500

    more dynamics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -21,6 +21,8 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel
   ],
 })
 export class HomePage {
+  endingHtml = '<h1>Done...</h1><p>And now my brain is fried. Oooops...</p>';
+  startingHtml = '<h1>Thinking...</h1><p>This is taking my brain a while. Hang on...</p>';
   theHtml = '<h1>HTML</h1><p>This is some strange HTML</p>';
   theState = 0;
 
