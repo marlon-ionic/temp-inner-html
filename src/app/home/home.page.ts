321424311ad713c7a2371bfd575bcf0415c1383e
Ken Sodemann <ken@ionic.io>
2024-07-29 13:04:09 -0500

    the base case works

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonLabel, IonItem, IonList, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  theHtml = '<h1>HTML</h1><p>This is some strange HTML</p>';
+
   constructor() {}
 }
