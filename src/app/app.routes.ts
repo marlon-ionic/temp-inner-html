4b328b30b1e2ef62c142d2838cb426efce465545
Marlon Harrison <marlon@ionic.io>
2024-07-30 18:14:18 -0500

    ionic g page example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'example',
+    loadComponent: () => import('./example/example.page').then( m => m.ExamplePage)
+  },
 ];
